65c528083323adeb09cc699b017c52d89e713544
safwan <lubdhak98@gmail.com>
2025-05-09 14:00:15 +0600

    fixed typo in router Changes to be committed: 	modified:   backend/routes/dragon.route.js

--- backend/routes/dragon.route.js
+++ backend/routes/dragon.route.js
@@ -1,22 +1,26 @@
-import express from 'express';
+import express from "express";
 
-import {createDragon, deleteDragon, getDragons, updateDragon} from "../controllers/dragon.controller.js";
+import {
+  createDragon,
+  deleteDragon,
+  getDragons,
+  updateDragon,
+} from "../controllers/dragon.controller.js";
 
 const router = express.Router();
-console.log('Registering dragon routes');
+console.log("Registering dragon routes");
 
-router.post("/", createDragon)
+router.post("/dragons", createDragon);
 
-router.put("/:id", updateDragon)
+router.put("/:id", updateDragon);
 
-router.delete("/:id", deleteDragon)
-
+router.delete("/:id", deleteDragon);
 
 // Add a specific API test endpoint
-router.get('/test', (req, res) => {
-    res.json({ message: 'Dragon API is working' });
+router.get("/test", (req, res) => {
+  res.json({ message: "Dragon API is working" });
 });
 
-router.get('/dragons', getDragons);
+router.get("/dragons", getDragons);
 
 export default router;
